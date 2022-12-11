44576b938357519d1b7bffe0877ac5c212a36270
Mohamed-Tazhiiroh <maxamedcumar513@gmail.com>
2022-12-11 12:19:30 +0300

    waan ku daray kayga

--- project/index.js
+++ project/index.js
@@ -5,4 +5,10 @@ dotenv.config();
 const port = process.env.port;
 const app = express();
 
+app.use(express.json());
+
+const prodRoute = require('./routes/productRoute')
+ 
+app.use('/api/Product/', prodRoute);
+
 app.listen(port, () => console.log(`server running on port ${port}`));
